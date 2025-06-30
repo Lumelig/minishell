bd297fc8abf8e675a10f080ad3c060c8794cf36a
jpflegha <pfleghaar.j@gmail.com>
2025-06-30 13:19:37 +0200

    start cmd_list

--- src/parsing.c
+++ src/parsing.c
@@ -1,16 +1,7 @@
 
 #include "minishell.h"
 
-t_cmd_list *init_cmd_list(void)
-{
-    t_cmd_list *cmd_list = malloc(sizeof(t_cmd_list));
-    if (!cmd_list)
-        return NULL;
-    cmd_list->head = NULL;
-    cmd_list->tail = NULL;
-    cmd_list->size = 0;
-    return cmd_list;
-}
+
 int is_operator(t_token *token)
 {
 	if (!token) 
@@ -44,11 +35,11 @@ t_cmd_list *parsing(t_env *my_env, t_token *token)
 {
     t_cmd_list *cmd_list;
     
-    cmd_list = init_cmd_list();
+    
 	expand_tokens(token, my_env->head, my_env);
     my_env->exit_status = syntax_validation(token);
     printf("value: %s Key: %d\n", token->value, token->type);
-    //token_to_cmd(token, cmd_list);
+    cmd_list = token_to_cmd(token, cmd_list);
 
     return(cmd_list);
 }
--- src/parsing/token_to_cmd.c
+++ src/parsing/token_to_cmd.c
@@ -1,11 +1,48 @@
 
 #include "minishell.h"
 
+t_cmd_list *init_cmd_list(void)
+{
+    t_cmd_list *cmd_list = malloc(sizeof(t_cmd_list));
+    if (!cmd_list)
+        return NULL;
+    cmd_list->head = NULL;
+    cmd_list->tail = NULL;
+    cmd_list->size = 0;
+    return cmd_list;
+}
+t_file_node *create_file_node(char *filename, int redir_type)
+{
+    t_file_node *node = malloc(sizeof(t_file_node));
+    if (!node)
+        return NULL;
+    
+    node->filename = strdup(filename);
+    node->redir_type = redir_type;
+    node->next = NULL;
+    return node;
+}
+t_cmd_node *create_cmd_node(void)
+{
+	t_cmd_node *node;
 
-void    token_to_cmd(t_token *token,  t_cmd_list *cmd_list)
+	node = malloc(sizeof(t_cmd_node));
+	if(!node)
+		return(NULL);
+	
+}
+
+
+t_cmd_list    token_to_cmd(t_token *token,  t_cmd_list *cmd_list)
 {
     (void)token;
-	if (!cmd_list)
+	if (!token)
 		return ;
-  
+	init_cmd_list;
+	 t_token *current;
+	 current = token;
+
+	
 }
+
+
