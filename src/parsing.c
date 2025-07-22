16dcd755a1027bfd8b73468025e71f39e75ef55e
jpflegha <pfleghaar.j@gmail.com>
2025-07-22 13:10:16 +0200

    first try of implement a syntax solution

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: jenne <jenne@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 01:30:11 by jenne             #+#    #+#             */
-/*   Updated: 2025/07/22 13:00:42 by jenne            ###   ########.fr       */
+/*   Updated: 2025/07/22 13:05:41 by jenne            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,7 @@ t_cmd_list	*parsing(t_env *my_env, t_token *token)
 	exit = syntax_validation(token);
 	if (exit > 0)
 		*exit_code() = 2;
-	cmd_list = token_to_cmd(token);
+	else
+		cmd_list = token_to_cmd(token);
 	return (cmd_list);
 }
