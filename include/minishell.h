a2d055b88dcfb8afe0410132803407403a94a5b2
jpflegha <pfleghaar.j@gmail.com>
2025-07-23 14:48:48 +0200

    norminette and added Pipe as file

--- include/minishell.h
+++ include/minishell.h
@@ -1,186 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jenne <jenne@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 13:45:59 by jenne             #+#    #+#             */
+/*   Updated: 2025/07/23 14:13:30 by jenne            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
 # include "execution.h"
+# include "parsing.h"
 # include "libft.h"
-# include <dirent.h> // opendir, readdir, closedir
-# include <fcntl.h>  // open
-# include <signal.h> // signal, sigaction, sigemptyset, sigaddset,
-// kill
-# include <stdio.h> // printf, perror
-//~~~
-# include <readline/history.h>  // add_history, rl_clear_history
-# include <readline/readline.h> // readline, rl_on_new_line, rl_replace_line,
-// rl_redisplay
+# include <dirent.h>        // opendir, readdir, closedir
+# include <fcntl.h>         // open
+# include <signal.h>        // signal, sigaction, sigemptyset, sigaddset, kill
 # include <stdbool.h>
-# include <stdlib.h>       // malloc, free, exit
-# include <string.h>       // strerror
-# include <sys/ioctl.h>    // ioctl
-# include <sys/resource.h> // struct rusage (used in wait3/wait4)
-# include <sys/stat.h>     // stat, lstat, fstat
-# include <sys/time.h>     // struct timeval (used in wait3/wait4)
-# include <sys/types.h>    // types for stat, wait
-# include <sys/wait.h>     // wait, waitpid, wait3, wait4
-# include <term.h>         // tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
-# include <termios.h>      // tcgetattr, tcsetattr
-# include <unistd.h>       // write, access, fork, getcwd, chdir, dup, dup2,
-							// pipe, isatty, ttyname, ttyslot
+# include <stdio.h>         // printf, perror
+# include <stdlib.h>        // malloc, free, exit
+# include <string.h>        // strerror
+# include <readline/history.h>  // add_history, rl_clear_history
+# include <readline/readline.h> // rl_on_new_line, rl_replace_line, rl_redisplay
+# include <sys/ioctl.h>     // ioctl
+# include <sys/resource.h>  // struct rusage (used in wait3/wait4)
+# include <sys/stat.h>      // stat, lstat, fstat
+# include <sys/time.h>      // struct timeval (used in wait3/wait4)
+# include <sys/types.h>     // types for stat, wait
+# include <sys/wait.h>      // wait, waitpid, wait3, wait4
+# include <term.h>          // tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+# include <termios.h>       // tcgetattr, tcsetattr
+# include <unistd.h>        // write, access, fork, getcwd, chdir, dup, dup2,
+// pipe, isatty, ttyname, ttyslot
 
-typedef enum
-{
-	TOKEN_WORD,         // e.g., echo, hello
-	TOKEN_PIPE,         // |
-	TOKEN_REDIR_IN,     // <
-	TOKEN_REDIR_OUT,    // >
-	TOKEN_REDIR_APPEND, // >>
-	TOKEN_HEREDOC,      // <<
-	TOKEN_EOF
-}							t_token_type;
-
-typedef struct s_token
-{
-	t_token_type			type;
-	char					*value;
-	struct s_token			*next;
-}							t_token;
-
-typedef struct s_file_node	t_file_node;
-// typedef struct s_cmd_node	t_cmd_node;
-typedef sig_atomic_t		sig_atomic_t;
-
-typedef struct s_file_list
-{
-	t_file_node				*head;
-	t_file_node				*tail;
-	ssize_t					size;
-}							t_file_list;
-
-typedef struct s_file_node
-{
-	char					*filename;
-	int						redir_type;
-	struct s_file_node		*next;
-}							t_file_node;
-
-typedef struct s_cmd_node
-{
-	int						cmd_type;
-	char					**cmd;
-	t_file_list				*files;
-	struct s_cmd_node		*next;
-}							t_cmd_node;
-typedef struct s_cmd_list
-{
-	t_cmd_node				*head;
-	t_cmd_node				*tail;
-	ssize_t					size;
-}							t_cmd_list;
-
-typedef struct s_quote_state
-{
-	int						in_single;
-	int						in_double;
-	int						continuation;
-}							t_quote_state;
-
+/* Environment structures */
 typedef struct s_envlist // char **env into a list
 {
-	char *key;
-	char delimiter;
-	char *value;
-	struct s_envlist *next;
+	char					*key;
+	char					delimiter;
+	char					*value;
+	struct s_envlist		*next;
 }							t_envlist;
 
 typedef struct s_env // Controling struct for env
 {
-	pid_t pid; // extra information put here for easy access
-	int size;
-	int shlvl; // help to handle an edge case
-	t_envlist *head;
-	t_envlist *tail;// same as for pid above
+	pid_t					pid; // extra information put here for easy access
+	int						size;
+	int						shlvl; // help to handle an edge case
+	t_envlist				*head;
+	t_envlist				*tail; // same as for pid above
 }							t_env;
 
-int 						*exit_code(void);
-void						process_quote_state(char *line, int *i,
-								t_quote_state *state);
-char						*get_continuation_prompt(int quote_type);
-t_cmd_list					*parsing(t_env *my_env, t_token *token);
+/* Global functions */
+int							*exit_code(void);
 void						setup_signal_handlers(void);
+
+/* Environment management functions */
+bool						add_env_var(t_env *env, char *key, char *value);
+bool						add_pwd_if_missing(t_env *my_env);
+bool						add_shlvl_if_missing(t_env *my_env);
+bool						check_required_vars(t_env *my_env);
+bool						init_default_env(t_env *my_env);
 bool						init_environment(t_env *my_env, char **env,
 								char **argv, int argc);
-t_token						*tokenize(char *line);
-char						*get_complete_input(void);
-t_quote_state				check_line_completion(char *line);
-int							check_quotes_balanced_enhanced(char *line);
-void						expand_tokens(t_token *token, t_envlist *envlist,
-								t_env *env);
-int copy_special_var(char *result, char *str, int *i, t_env *env);
-int	copy_variable(char *result, char *str, int *i, t_envlist *envlist);
-int							get_special_var_skip(char *str, int i);
-int							is_special_var(char *str, int pos);
-int							is_special_expansion(char *str, int i);
-int							calculate_var_size(char *str, int i,
-								t_envlist *envlist, t_env *env);
-int							calculate_special_var_size(char *str, int i,
-								t_env *env);
-int							get_var_length(char *str, int start, int *end_pos);
+bool						process_env_vars(t_env *my_env, char **env);
+bool						update_or_add_env_var(t_env *env, char *key,
+								char *value);
+char						*create_env_string(t_envlist *current);
+char						*get_env_value(t_env *env, const char *key);
+char						**env_to_array(t_env *env);
+t_envlist					*find_env_var(t_env *env, const char *key);
+void						cleanup_env(t_env *env);
+void						free_env_array(char **env_array, int count);
 void						free_environment(t_env *my_env);
-t_cmd_list					*token_to_cmd(t_token *token);
-void						print_cmd_list(t_cmd_list *cmd_list);
-
 void						init_empty_env(t_env *env, char **argv, int argc);
 void						set_environment(t_env *my_env, char **key,
 								char **value, char *env);
-bool						init_default_env(t_env *my_env);
-bool						process_env_vars(t_env *my_env, char **env);
-bool						init_environment(t_env *my_env, char **env,
-								char **argv, int argc);
-
-// env_manage.c functions
-bool						add_env_var(t_env *env, char *key, char *value);
-t_envlist					*find_env_var(t_env *env, const char *key);
-bool						update_or_add_env_var(t_env *env, char *key,
-								char *value);
-char						*get_env_value(t_env *env, const char *key);
-void						cleanup_env(t_env *env);
-
-// env_validate.c functions
-bool						add_shlvl_if_missing(t_env *my_env);
-bool						add_pwd_if_missing(t_env *my_env);
-bool						check_required_vars(t_env *my_env);
-char						*create_env_string(t_envlist *current);
-void						free_env_array(char **env_array, int count);
-
-// env_convert.c functions
-char						**env_to_array(t_env *env);
-
-/* tokenizer_utils.c */
-t_token	*cleanup_tokens(t_token *head);
-t_token	*create_new_token(t_token_type type, const char *value);
-t_token	*find_last_token(t_token *head);
-int		add_token(t_token **head, t_token_type type, const char *value);
-int		is_operator_char(char c);
-
-/* tokenizer_escape.c */
-char	*handle_escape_char(char c);
-int		append_escaped_char(char *line, int *i, char **result);
-int		append_regular_char(char *line, int *i, char **result);
-int		handle_quoted_content(char *line, int *i, char **result, char quote);
-int		handle_quote_in_word(char *line, int *i, char **word);
-
-/* tokenizer_word.c */
-int		handle_escape_in_word(char *line, int *i, char **word);
-int		handle_regular_char_in_word(char *line, int *i, char **word);
-int		extract_word(char *line, int *i, char **word);
-int		process_word_token(char *line, int *i, t_token **head);
-
-/* tokenizer_operators.c */
-int		handle_pipe_operator(int *i, t_token **head);
-int		handle_output_redirect(char *line, int *i, t_token **head);
-int		handle_input_redirect(char *line, int *i, t_token **head);
-int		handle_operator(char *line, int *i, t_token **head);
-
-/* tokenizer.c */
-t_token	*tokenize(char *line);
 
 #endif
---
+++ src/parsing/cmd_utils.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jenne <jenne@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 14:43:36 by jenne             #+#    #+#             */
+/*   Updated: 2025/07/23 14:43:54 by jenne            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_cmd_list	*init_cmd_list(void)
+{
+	t_cmd_list	*cmd_list;
+
+	cmd_list = malloc(sizeof(t_cmd_list));
+	if (!cmd_list)
+		return (NULL);
+	cmd_list->head = NULL;
+	cmd_list->tail = NULL;
+	cmd_list->size = 0;
+	return (cmd_list);
+}
+
+t_file_list	*init_file_list(void)
+{
+	t_file_list	*file_list;
+
+	file_list = malloc(sizeof(t_file_list));
+	if (!file_list)
+		return (NULL);
+	file_list->head = NULL;
+	file_list->size = 0;
+	file_list->tail = NULL;
+	return (file_list);
+}
+
+t_file_node	*create_file_node(char *filename, t_token_type redir_type)
+{
+	t_file_node	*node;
+
+	node = malloc(sizeof(t_file_node));
+	if (!node)
+		return (NULL);
+	if (redir_type == TOKEN_PIPE)
+		node->filename = "PIPE";
+	else
+		node->filename = strdup(filename);
+	if (!node->filename)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->redir_type = redir_type;
+	node->next = NULL;
+	return (node);
+}
