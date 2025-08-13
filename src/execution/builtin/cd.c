445feb3d423f02e7369270746ad0ab983d4a904e
maxrmax <mring@student.42heilbronn.de>
2025-08-13 13:02:16 +0200

    Builtins finished for now

--- src/execution/builtin/cd.c
+++ src/execution/builtin/cd.c
@@ -1,8 +1,69 @@
 #include "minishell.h"
 
-void	cd_builtin(t_cmd_node *curr, t_env *env)
+static void	update_oldpwd(t_env *ms_env)
 {
-	(void)curr;
-	(void)env;
-	printf("TODO: cd\n");
+	char	*old_pwd;
+
+	old_pwd = getcwd(NULL, 0);
+	if (!old_pwd)
+		return ;
+	if (!env_key_update(ms_env, "OLDPWD", old_pwd))
+		if (!insert_env_node(ft_strdup("OLDPWD"), old_pwd, ms_env))
+		{
+			free(old_pwd);
+			printf("failed to insert oldpwd\n");
+		}
+}
+
+static void	update_pwd(t_env *ms_env)
+{
+	char	*new_pwd;
+
+	new_pwd = getcwd(NULL, 0);
+	if (!new_pwd)
+		return ;
+	if (!env_key_update(ms_env, "PWD", new_pwd))
+		if (!insert_env_node(ft_strdup("PWD"), new_pwd, ms_env))
+		{
+			free(new_pwd);
+			printf("cd: failed to update PWD\n");
+		}
+}
+
+static char	*get_target_directory(t_cmd_node *curr, t_env *ms_env)
+{
+	char	*dir;
+
+	if (curr->cmd[1] == NULL)
+	{
+		dir = get_env_value(ms_env, "HOME");
+		if (!dir)
+		{
+			printf("cd: HOME not set\n");
+			return (NULL);
+		}
+		return (dir);
+	}
+	return (curr->cmd[1]);
+}
+
+void	cd_builtin(t_cmd_node *curr, t_env *ms_env)
+{
+	char	*target_dir;
+
+	if (curr->cmd[2])
+	{
+		printf("cd: too many arguments\n");
+		return ;
+	}
+	update_oldpwd(ms_env);
+	target_dir = get_target_directory(curr, ms_env);
+	if (!target_dir)
+		return ;
+	if (chdir(target_dir) != 0)
+	{
+		perror("cd");
+		return ;
+	}
+	update_pwd(ms_env);
 }
--- src/execution/builtin/unset.c
+++ src/execution/builtin/unset.c
@@ -1,8 +1,39 @@
 #include "minishell.h"
 
-void	unset_builtin(t_cmd_node *curr, t_env *env)
+static void	remove_env_node(t_envlist *node_to_remove, t_env *ms_env)
 {
-	(void)curr;
-	(void)env;
-	printf("TODO: unset\n");
+	t_envlist	*current;
+
+	if (ms_env->head == node_to_remove)
+		ms_env->head = node_to_remove->next;
+	else
+	{
+		current = ms_env->head;
+		while (current->next != node_to_remove)
+			current = current->next;
+		current->next = node_to_remove->next;
+		if (ms_env->tail == node_to_remove)
+			ms_env->tail = current;
+	}
+	if (ms_env->tail == node_to_remove && ms_env->head == node_to_remove->next)
+		ms_env->tail = NULL;
+	free(node_to_remove->value);
+	free(node_to_remove->key);
+	free(node_to_remove);
+	ms_env->size--;
+}
+
+void	unset_builtin(t_cmd_node *curr, t_env *ms_env)
+{
+	int			i;
+	t_envlist	*node_to_remove;
+
+	i = 1;
+	while (curr->cmd[i])
+	{
+		node_to_remove = find_env_var(ms_env, curr->cmd[i]);
+		if (node_to_remove)
+			remove_env_node(node_to_remove, ms_env);
+		i++;
+	}
 }
