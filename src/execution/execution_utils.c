5e64090b1aaf407f34464ca22c5ea97b231043a4
maxrmax <ringmaximilian@protonmail.com>
2025-08-29 16:48:41 +0200

    execve implemented. TODO: piping, here_doc, norminette clean_up

--- src/execution/execution_utils.c
+++ src/execution/execution_utils.c
@@ -4,6 +4,8 @@ void	free_envp(char **envp)
 {
 	int	i;
 
+	if (!envp)
+		return ;
 	i = 0;
 	while (envp[i])
 	{
@@ -20,19 +22,18 @@ char	**env_convert(t_env *ms_env)
 	int			i;
 	int			key_len;
 
-	i = 0;
-	curr = ms_env->head;
+	// do i need ms_env null check?
 	envp = malloc(sizeof(char *) * (ms_env->size + 1));
 	if (!envp)
 		return (NULL);
+	i = 0;
+	curr = ms_env->head;
 	while (curr)
 	{
 		key_len = ft_strlen(curr->key);
 		envp[i] = malloc(key_len + ft_strlen(curr->value) + 2);
 		if (!envp[i])
-		{
 			return (free_envp(envp), NULL);
-		}
 		ft_strcpy(envp[i], curr->key);
 		envp[i][key_len] = '=';
 		ft_strcpy(envp[i] + key_len + 1, curr->value);
@@ -42,11 +43,65 @@ char	**env_convert(t_env *ms_env)
 	return (envp[i] = NULL, envp);
 }
 
-char	*get_exec_path(t_env *ms_env)
+void	free_split_path(char **split_path)
 {
-	char *path;
+	int	i;
 
-	path = get_env_value(ms_env, "PATH");
-	// find correct path for the executable
-	return (path);
+	// do i need null check?
+	i = 0;
+	while (split_path[i])
+		free(split_path[i++]);
+	free(split_path);
+	return ;
+}
+
+char	*cmd_path_append(char *path, char *cmd)
+{
+	char	*joined;
+	int		len_1;
+	int		len_2;
+
+	// path/cmd null check?
+	len_1 = ft_strlen(path);
+	len_2 = ft_strlen(cmd);
+	joined = malloc(len_1 + 1 + len_2 + 1);
+	if (!joined)
+		return (NULL);
+	ft_strcpy(joined, path);
+	joined[len_1] = '/';
+	ft_strcpy(joined + len_1 + 1, cmd);
+	joined[len_1 + len_2 + 1] = '\0';
+	return (joined);
+}
+
+char	*get_exec_path(t_cmd_node *curr, t_env *ms_env)
+{
+	char	**split_path;
+	char	*full_path;
+	char	*appended;
+	int		i;
+
+	// null check? curr, curr->cmd, curr->cmd[0]
+	if (ft_strchr(curr->cmd[0], '/'))
+	{
+		if (access(curr->cmd[0], F_OK | X_OK) == 0)
+			return (curr->cmd[0]);
+		return (NULL);
+	}
+	full_path = get_env_value(ms_env, "PATH");
+	if (!full_path || !*full_path)
+		return (NULL);
+	split_path = ft_split(full_path, ':');
+	if (!split_path)
+		return (NULL);
+	i = 0;
+	while (split_path[i])
+	{
+		appended = cmd_path_append(split_path[i], curr->cmd[0]);
+		if (appended && access(appended, F_OK | X_OK) == 0)
+			return (free_split_path(split_path), appended);
+		free(appended);
+		i++;
+	}
+	return (free_split_path(split_path), NULL);
 }
