0b43f7408ee29dbe0a0330bc2fc981e22de94e71
maxrmax <ringmaximilian@protonmail.com>
2025-09-12 17:49:20 +0200

    Implemented: execution chain with pipes and redirects fully working. Had to adjust builtins for pipe chains -> Split run_builtin into check_builtin and run_builtin again. Had to comment token_pipe out of redir_type in token_to_cmd.c - made redirections a subfunction to be used in builtins and in handle_child(); TODO: heredoc, signal interrupts, some minor rewrites for norm. Testing at the end

--- src/execution/fork_pipeline.c
+++ src/execution/fork_pipeline.c
@@ -22,15 +22,68 @@ void	wait_children(pid_t *pid, ssize_t child_count)
 	}
 }
 
-// set up child pipes
-void	handle_child(t_cmd_node *curr, t_env *ms_env)
+void	handle_child(t_cmd_node *curr, t_cmd_node *prev, t_env *ms_env,
+		t_cmd_list *cmd_list)
 {
-	exec_cmd(curr, ms_env);
+	// cmd2 reads from file, writes to file
+	// cmd1 | cmd2 < input.txt > output.txt | cmd3
+	// Last redirection wins (file2.txt)
+	// cmd1 | cmd2 > file1.txt > file2.txt
+	// file2.txt won
+	// Connect stdin to previous command's output
+	if (prev && prev->p_fd[0] != -1)
+	{
+		dup2(prev->p_fd[0], STDIN_FILENO);
+		// stdin now reads from prev pipe
+	}
+	// Connect stdout to current command's pipe
+	if (curr->next && curr->p_fd[1] != -1)
+	{
+		dup2(curr->p_fd[1], STDOUT_FILENO);
+		// stdout now writes to curr pipe
+	}
+	// Handle file redirections (can override pipes)
+	// Can override pipe stdin
+	handle_redirections(curr);
+	// close leftover pipes, but which ones?
+	if (prev && prev->p_fd[0] != -1)
+		close(prev->p_fd[0]);
+	if (prev && prev->p_fd[1] != -1)
+		close(prev->p_fd[1]);
+	if (curr->p_fd[0] != -1)
+		close(curr->p_fd[0]);
+	if (curr->p_fd[1] != -1)
+		close(curr->p_fd[1]);
+	if (curr->builtin_type)
+		run_builtin(curr, ms_env, cmd_list);
+	else
+		exec_cmd(curr, ms_env);
 }
 
-// set up parent pipes
-void	handle_parent(t_cmd_node *curr, t_env *ms_env)
+void	handle_parent(t_cmd_node *curr, t_cmd_node *prev)
 {
-	(void)curr;
-	(void)ms_env;
+	// close write end of current child/pipe
+	if (curr->next && curr->p_fd[1] != -1)
+	{
+		close(curr->p_fd[1]);
+		curr->p_fd[1] = -1;
+	}
+	// close read end of previous pipe
+	if (prev && prev->p_fd[0] != -1)
+	{
+		close(prev->p_fd[0]);
+		prev->p_fd[0] = -1;
+	}
 }
+
+/*
+echo hello world | cat < /dev/null | wc -l
+
+ls -la | grep ".txt" > files.txt | wc -l
+
+echo "test1" | cat > output1.txt > output2.txt | echo "test2"
+
+cat < /etc/passwd | head -5 > users.txt | tail -2 > last_users.txt | wc
+
+echo "line1" | tee file.txt | cat >> file.txt | wc -c
+*/
--- src/execution/redirections.c
+++ src/execution/redirections.c
@@ -1,1 +1,57 @@
 #include "minishell.h"
+
+void	handle_redirections(t_cmd_node *curr)
+{
+	t_file_node	*file;
+
+	if (curr->files && curr->files->head)
+	{
+		file = curr->files->head;
+		while (file)
+		{
+			if (file->redir_type == TOKEN_REDIR_IN)
+				handle_input(file->filename);
+			else if (file->redir_type == TOKEN_HEREDOC)
+				handle_heredoc(file->filename);
+			else if (file->redir_type == TOKEN_REDIR_OUT)
+				handle_output(file->filename, false);
+			else if (file->redir_type == TOKEN_REDIR_APPEND)
+				handle_output(file->filename, true);
+			file = file->next;
+		}
+	}
+}
+
+void	handle_input(char *infile)
+{
+	int	fd;
+
+	fd = open(infile, O_RDONLY);
+	if (fd == -1)
+	{
+		perror(infile);
+		exit(1);
+	}
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+}
+
+void	handle_output(char *outfile, bool append)
+{
+	int	flags;
+	int	fd;
+
+	flags = O_WRONLY | O_CREAT;
+	if (append)
+		flags |= O_APPEND;
+	else
+		flags |= O_TRUNC;
+	fd = open(outfile, flags, 0644);
+	if (fd == -1)
+	{
+		perror(outfile);
+		exit(1);
+	}
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+}
