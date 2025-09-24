ce898094e135d2e4a6c07bced987a9313cb89b7b
jpflegha <pfleghaar.j@gmail.com>
2025-09-24 12:43:13 +0200

    try to fix expand'

--- src/parsing/expand.c
+++ src/parsing/expand.c
@@ -3,42 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   expand.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jenne <jenne@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jpflegha <jpflegha@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 01:23:20 by jenne             #+#    #+#             */
-/*   Updated: 2025/09/22 14:33:03 by jenne            ###   ########.fr       */
+/*   Updated: 2025/09/24 12:40:30 by jpflegha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-bool	check_quoting(char *original)
+void	check_quoting(char *original, char **join, char **expand, int *i)
 {
 	t_quote quote_state;
-	int		i;
+	int		start;
+	int		start2;
 
-	i = 0;
+	start2 = *i;
 	quote_state = QUOTE_NONE;
-	while (original[i])
+	*join = NULL;  // Initialize join to NULL
+	*expand = NULL;  // Initialize expand to NULL
+	
+	while (original[*i])
 	{
-		if (original[i] == '"')
+		if (original[*i] == '"')
 		{
 			if (quote_state == QUOTE_DOUBLE)
+			{
 				quote_state = QUOTE_NONE;
+			}
 			else if (quote_state == QUOTE_NONE)
+			{
 				quote_state = QUOTE_DOUBLE;
+			}
 		}
-		else if (original[i] == '\'')
+		else if (original[*i] == '\'')
 		{
 			if (quote_state == QUOTE_NONE)
 			{
-				if (i == 0 || original[i - 1] != '\\')
-					return true;
+				start = *i;  
+				quote_state = QUOTE_SINGLE;	
+			}
+			else if (quote_state == QUOTE_SINGLE)
+			{
+				*expand = ft_substr(original, start2, start - start2);
+				*join = ft_substr(original, start + 1, *i - start - 1);  // Content inside quotes
+				(*i)++;  // Move past the closing quote
+				return ;
 			}
 		}
-		i++;
+		(*i)++;
 	}
-	return false;
+	
+	// If no single quote pair found, expand the entire remaining string
+	*expand = ft_substr(original, start2, *i - start2);
+	*join = NULL;
 }
 
 static int	handle_variable_size(char *original, int *i, t_env *env,
@@ -63,7 +81,7 @@ static int	handle_variable_size(char *original, int *i, t_env *env,
 		*i = var_end - 1;
 		return (size);
 	}
-	return (0);
+	return (1);  // Return 1 for literal '$' character
 }
 
 int	calculate_expanded_size(char *original, t_env *env, t_envlist *envlist)
@@ -117,16 +135,74 @@ static void	process_expansion(char *result, char *original, t_envlist *envlist,
 char	*expand_string(char *original, t_envlist *envlist, t_env *env)
 {
 	char	*result;
+	char	*expand;
+	char	*fin_result;
+	char	*join;
+	char	*temp;
 	int		result_size;
+	int 	i;
 
-	if (!original || !ft_strchr(original, '$'))
-		return (ft_strdup(original));
-	else if (check_quoting(original))
-		return (ft_strdup(original));
-	result_size = calculate_expanded_size(original, env, envlist);
-	result = malloc(result_size);
-	if (!result)
+	if (!original)
 		return (NULL);
-	process_expansion(result, original, envlist, env);
-	return (result);
+	if (!ft_strchr(original, '$'))
+		return (ft_strdup(original));
+		
+	i = 0;
+	fin_result = ft_strdup("");  // Initialize with empty string
+	if (!fin_result)
+		return (NULL);
+		
+	while (original[i])
+	{
+		check_quoting(original, &join, &expand, &i);
+		
+		if (!expand)  // Error in check_quoting
+		{
+			free(fin_result);
+			return (NULL);
+		}
+		
+		result_size = calculate_expanded_size(expand, env, envlist);
+		result = malloc(result_size);
+		if (!result)
+		{
+			free(expand);
+			free(join);
+			free(fin_result);
+			return (NULL);
+		}
+		
+		process_expansion(result, expand, envlist, env);
+		
+		// Join the expanded result
+		temp = ft_strjoin(fin_result, result);
+		free(fin_result);
+		free(result);
+		fin_result = temp;
+		
+		if (!fin_result)
+		{
+			free(expand);
+			free(join);
+			return (NULL);
+		}
+		
+		// Join the literal content (from inside single quotes)
+		if (join)
+		{
+			temp = ft_strjoin(fin_result, join);
+			free(fin_result);
+			free(join);
+			fin_result = temp;
+			if (!fin_result)
+			{
+				free(expand);
+				return (NULL);
+			}
+		}
+		
+		free(expand);
+	}
+	
+	return (fin_result);
 }
